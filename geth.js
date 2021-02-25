4b302a1a2cbe7e4ed27ec42e60c2d0ce4709c21e
sam <sam@freighttrust.com>
2021-02-25 11:40:38 -0800

    conformance
    Signed-off-by: sam <sam@freighttrust.com>

--- geth.js
+++ geth.js
@@ -1,11 +1,11 @@
-// @geth tx 
+// @geth tx
 
 'use strict';
 
 const fs = require('fs');
 const argv = require('yargs').argv;
-const { Writable } = require('stream');
-const Plotly  = require('plotly');
+const {Writable} = require('stream');
+const Plotly = require('plotly');
 
 const INTERVAL = (argv.interval || 30) * 1000;
 
@@ -16,33 +16,54 @@ if (process.env.PLOT_USER && process.env.PLOT_PWD) {
 
 const input = argv.input;
 if (!input) {
-  console.error('Input log file required. Use "--input" to tell the program which file to process.');
+  console.error(
+    'Input log file required. Use "--input" to tell the program which file to process.',
+  );
   process.exit(1);
 }
 
 const src = fs.createReadStream(input);
 
-let pooltimes = [], executables = [], nonexecutables = [], stales = [], txtimes = [], txs = [];
+let pooltimes = [],
+  executables = [],
+  nonexecutables = [],
+  stales = [],
+  txtimes = [],
+  txs = [];
 const outStream = new Writable({
   write(chunk, encoding, callback) {
     let lines = chunk.toString();
-    let matches = lines.match(/\[([0-9]{2}-[0-9]{2}\|[0-9]{2}:[0-9]{2}:[0-9]{2})\]\s+Transaction pool status report\s+executable=(\d+)\s+queued=(\d+)\s+stales=(\d+)/g);
+    let matches = lines.match(
+      /\[([0-9]{2}-[0-9]{2}\|[0-9]{2}:[0-9]{2}:[0-9]{2})\]\s+Transaction pool status report\s+executable=(\d+)\s+queued=(\d+)\s+stales=(\d+)/g,
+    );
     if (matches) {
       // there should be more than one matches, now parse each match to get the details
-      for (let i=0; i<matches.length; i++) {
-        let one = matches[i].match(/\[([0-9]{2}-[0-9]{2}\|[0-9]{2}:[0-9]{2}:[0-9]{2})\]\s+Transaction pool status report\s+executable=(\d+)\s+queued=(\d+)\s+stales=(\d+)/);
+      for (let i = 0; i < matches.length; i++) {
+        let one = matches[i].match(
+          /\[([0-9]{2}-[0-9]{2}\|[0-9]{2}:[0-9]{2}:[0-9]{2})\]\s+Transaction pool status report\s+executable=(\d+)\s+queued=(\d+)\s+stales=(\d+)/,
+        );
         pooltimes.push(one[1]);
         executables.push(parseInt(one[2]));
         nonexecutables.push(parseInt(one[3]));
         stales.push(parseInt(one[4]));
-        console.log('Pool at %s: executable - %s, \tnon-executable - %s, \tstale - %s', one[1], one[2], one[3], one[4]);
+        console.log(
+          'Pool at %s: executable - %s, \tnon-executable - %s, \tstale - %s',
+          one[1],
+          one[2],
+          one[3],
+          one[4],
+        );
       }
     }
 
-    matches = lines.match(/\[([0-9]{2}-[0-9]{2}\|[0-9]{2}:[0-9]{2}:[0-9]{2})\]\s+(Inserted new block|Commit new mining work).+txs=(\d+)/g);
+    matches = lines.match(
+      /\[([0-9]{2}-[0-9]{2}\|[0-9]{2}:[0-9]{2}:[0-9]{2})\]\s+(Inserted new block|Commit new mining work).+txs=(\d+)/g,
+    );
     if (matches) {
-      for (let i=0; i<matches.length; i++) {
-        let one = matches[i].match(/\[([0-9]{2}-[0-9]{2}\|[0-9]{2}:[0-9]{2}:[0-9]{2})\]\s+(Inserted new block|Commit new mining work).+txs=(\d+)/);
+      for (let i = 0; i < matches.length; i++) {
+        let one = matches[i].match(
+          /\[([0-9]{2}-[0-9]{2}\|[0-9]{2}:[0-9]{2}:[0-9]{2})\]\s+(Inserted new block|Commit new mining work).+txs=(\d+)/,
+        );
         txtimes.push(one[1]);
         txs.push(parseInt(one[3]));
         console.log('Txs at %s: %s', one[1], one[3]);
@@ -50,38 +71,39 @@ const outStream = new Writable({
     }
 
     callback();
-  }
+  },
 });
 
-src.pipe(outStream)
-.on('finish', () => {
+src.pipe(outStream).on('finish', () => {
   let exec = {
     x: pooltimes,
     y: executables,
     name: 'txpool - executables',
-    type: 'scatter'
+    type: 'scatter',
   };
 
   let nonexec = {
     x: pooltimes,
     y: nonexecutables,
     name: 'txpool - non-executables',
-    type: 'scatter'
+    type: 'scatter',
   };
 
   let stale = {
     x: pooltimes,
     y: stales,
     name: 'txpool - stales',
-    type: 'scatter'
+    type: 'scatter',
   };
 
-  let averageTimestamps = [], averageTimes = [], averageTxs = [];
-  for (let i=0; i<txtimes.length; i++) {
+  let averageTimestamps = [],
+    averageTimes = [],
+    averageTxs = [];
+  for (let i = 0; i < txtimes.length; i++) {
     // timestamp format is mm-dd|hh:mm:ss
     let matches = txtimes[i].match(/([0-9]{2})-([0-9]{2})\|([0-9]{2}):([0-9]{2}):([0-9]{2})/);
     let ts = new Date();
-    ts.setMonth(parseInt(matches[1]-1));
+    ts.setMonth(parseInt(matches[1] - 1));
     ts.setDate(parseInt(matches[2]));
     ts.setHours(parseInt(matches[3]));
     ts.setMinutes(parseInt(matches[4]));
@@ -107,25 +129,25 @@ src.pipe(outStream)
     x: txtimes,
     y: txs,
     name: 'Tx per block',
-    type: 'scatter'
+    type: 'scatter',
   };
 
   let averageTx = {
     x: averageTimestamps,
     y: averageTxs,
-    name: `Aggregate Tx per ${INTERVAL/1000} seconds`,
-    type: 'scatter'
+    name: `Aggregate Tx per ${INTERVAL / 1000} seconds`,
+    type: 'scatter',
   };
 
-  plotly.plot([tx, averageTx], {filename: "geth-logs-txrate"}, function (err, msg) {
+  plotly.plot([tx, averageTx], {filename: 'geth-logs-txrate'}, function (err, msg) {
     if (err) return reject(err);
 
     console.log('Chart available at: %s', msg.url);
   });
 
-  plotly.plot([exec, nonexec, stale], {filename: "geth-logs-txpool"}, function (err, msg) {
+  plotly.plot([exec, nonexec, stale], {filename: 'geth-logs-txpool'}, function (err, msg) {
     if (err) return reject(err);
 
     console.log('Chart available at: %s', msg.url);
   });
-});;
+});
